bde9a65a67837e7ab3b9086f28c53b464bcb229f
Yuri Veras Leandro <yurivl@hotmail.com>
2022-05-18 19:13:18 -0300

    Save score

--- backend/src/main/java/com/devsuperior/dsmovie/controllers/MovieController.java
+++ backend/src/main/java/com/devsuperior/dsmovie/controllers/MovieController.java
@@ -5,30 +5,34 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.devsuperior.dsmovie.dto.MovieDTO;
+import com.devsuperior.dsmovie.dto.ScoreDTO;
 import com.devsuperior.dsmovie.services.MovieService;
 
 @RestController
-@RequestMapping(value = "/movies")
+@RequestMapping(value = "/movie")
 public class MovieController {
-	
-	@Autowired
-	private MovieService service;
-	
-	
-	@GetMapping
-	public Page<MovieDTO> findAll(Pageable pageable){
-		return service.findAll(pageable);
 		
-	}
-	
-	@GetMapping(value = "/{id}")
-	public MovieDTO findById(@PathVariable long id){
-		return service.findById(id);
 		
-	}
-
+		@Autowired
+		private MovieService service;
+		
+		
+		@GetMapping
+		public Page<MovieDTO> findAll(Pageable pageable){
+			return service.findAll(pageable);
+			
+		}
+		
+		@GetMapping(value = "/{id}")
+		public MovieDTO findById(@PathVariable long id){
+			return service.findById(id);
+			
+		}
+		
 }
